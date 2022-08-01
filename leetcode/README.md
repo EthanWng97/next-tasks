bfdcf588f79ff53692a97755d8c8806b95c15653
Yifan Wang <yif.wang@icloud.com>
2022-07-31 18:32:09 -0700

    feat: add leetcode folder

---
+++ leetcode/README.md
@@ -0,0 +1,50 @@
+# Leetcode Today Record（每日一题）
+
+## Usage
+- (推荐 `Recommend`)通过 RSS 进行订阅 
+**Subscribe via RSS**
+
+    [CN Version](https://rss-evan.herokuapp.com/leetcode/todayrecord/cn) | [EN Version](https://rss-evan.herokuapp.com/leetcode/todayrecord/en)
+
+<center class="half">
+    <img src="https://raw.githubusercontent.com/NavePnow/leetcode-today-record/main/img/rss-cn.png" width="50%"/><img src="https://raw.githubusercontent.com/NavePnow/leetcode-today-record/main/img/rss-en.png" width="50%"/>
+</center>
+
+- 通过 [Telegram Channel](https://t.me/leetcodeTodayRecord) 进行订阅 
+**Subscribe via [Telegram Channel](https://t.me/leetcodeTodayRecord)**
+<img src="https://raw.githubusercontent.com/NavePnow/leetcode-today-record/main/img/telegram.png" width="40%">
+
+
+## Author
+
+👤 **Evan**
+
+* Twitter: [@NavePnow](https://twitter.com/NavePnow)
+* Github: [@NavePnow](https://github.com/NavePnow)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+Feel free to check [issues page]().
+
+## 💰 Show your support
+
+Give a ⭐️ if this project helped you!
+
+| PayPal                                                                                                                                                                       | Patron                                                                                                    |
+| ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------- |
+| [![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=DSZJCN4ZUEW74&currency_code=USD&source=url) |   <a href="https://www.patreon.com/NavePnow"> <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160"> </a>
+
+## 📖 Reference
+[Leetcode CN Site](https://leetcode-cn.com)
+[Leetcode EN Site](https://leetcode.com)
+
+## 🙏 Acknowledgments
+* [RSSHub](https://docs.rsshub.app)
+
+
+## 📝 License
+
+---
+Copyright © 2020 [Evan](https://github.com/NavePnow).
+This project is MIT licensed.
---
+++ leetcode/google-script/leetcode-cn.js
@@ -0,0 +1,178 @@
+var token = "";
+var chat_id = "@leetcodeDailyQuestion";
+
+var telegramHost = "https://api.telegram.org/bot" + token;
+
+var leetcodeHost = "https://leetcode.cn";
+
+var question = {
+  date: "",
+  sourceLink: "",
+  solutionLink: "",
+  titleSlug: "",
+  content: "",
+  frontedId: "",
+  difficulty: "",
+  tags: "",
+  file: "",
+};
+
+function getAllImageTags(str) {
+  let words = [];
+  str.replace(/<img[^>]+src="([^">]+)"/g, function ($0, $1) {
+    words.push($1);
+  });
+  return words;
+}
+
+function getDailyQuestion() {
+  const dailyQuestionPayload = {
+    query: `query questionOfToday {
+            todayRecord {
+                date
+                question {
+                    frontendQuestionId: questionFrontendId
+                    titleSlug
+                }
+            }
+        } `,
+    variables: {},
+  };
+
+  var options = {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    payload: JSON.stringify(dailyQuestionPayload),
+  };
+
+  var dailyQuestionResponse = UrlFetchApp.fetch(
+    leetcodeHost + "/graphql",
+    options
+  );
+  const data = JSON.parse(dailyQuestionResponse).data.todayRecord[0];
+  question.date = data.date;
+  // question.sourceLink = leetcodeHost + data.link;
+  question.solutionLink = question.sourceLink + "solution";
+  question.titleSlug = data.question.titleSlug;
+  question.sourceLink = leetcodeHost + "/problems/" + question.titleSlug;
+  question.solutionLink = question.sourceLink + "/solution";
+  getDetails();
+}
+
+function getDetails() {
+  const detailsPayload = {
+    operationName: "questionData",
+    query: `query questionData($titleSlug: String!) {
+            question(titleSlug: $titleSlug) {
+                questionId
+                questionFrontendId
+                title
+                titleSlug
+                content
+                translatedTitle
+                translatedContent
+                difficulty
+                topicTags {
+                    name
+                    slug
+                    translatedName
+                    __typename
+                }
+                __typename
+            }
+        }`,
+    variables: {
+      titleSlug: question.titleSlug,
+    },
+  };
+
+  var options = {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    payload: JSON.stringify(detailsPayload),
+  };
+  var detailsResponse = UrlFetchApp.fetch(leetcodeHost + "/graphql", options);
+  const emoji = {
+    Medium: "🟡",
+    Easy: "🟢",
+    Hard: "🔴",
+  };
+  const details = JSON.parse(detailsResponse).data.question;
+  question.content = details.translatedContent;
+  question.frontedId = details.questionFrontendId;
+  question.difficulty = emoji[details.difficulty];
+
+  // handle images
+
+  const images = getAllImageTags(question.content);
+  images.forEach((image) => {
+    const res = UrlFetchApp.fetch(image);
+    const imgbase64 =
+      "data:" +
+      res.getBlob().getContentType() +
+      ";base64," +
+      Utilities.base64Encode(res.getBlob().getBytes());
+    // question.content = replaceAll(question.content, image, imgbase64);
+    question.content = question.content.replace(image, imgbase64);
+  });
+
+  const blob = Utilities.newBlob(question.content, MimeType.HTML);
+  blob.setName(question.frontedId + "." + question.titleSlug + ".pdf");
+  question.file = blob.getAs(MimeType.PDF);
+
+  let topicTags = details.topicTags;
+  topicTags = topicTags.map((item) => {
+    let slug = "#" + item.slug;
+    slug = slug.replaceAll("-", "_");
+    return slug;
+  });
+  question.tags = topicTags.join(" ");
+  originalData();
+}
+
+function originalData() {
+  const caption =
+    "<b>Leetcode.cn " +
+    question.date +
+    "</b>\n" +
+    "<b>" +
+    question.difficulty +
+    question.frontedId +
+    "." +
+    question.titleSlug +
+    "</b>\n\n" +
+    "<strong>🏷️ Tags\n</strong>" +
+    question.tags;
+  var payload = {
+    method: "sendDocument",
+    chat_id: chat_id,
+    document: question.file,
+    caption: caption,
+    parse_mode: "Html",
+    reply_markup:
+      '{"inline_keyboard" : [' +
+      "[" +
+      '{"text":"来源", "url" : "' +
+      question.sourceLink +
+      '"},' +
+      '{"text":"答案", "url" : "' +
+      question.solutionLink +
+      '"}' +
+      "]" +
+      "]}",
+  };
+  sendMsg(payload);
+}
+
+function sendMsg(payload) {
+  var options = {
+    method: "post",
+    payload: payload,
+  };
+
+  UrlFetchApp.fetch(telegramHost + "/", options);
+}
