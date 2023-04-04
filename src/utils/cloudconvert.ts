e8a4d67d93eeb3b36fbb88f883e14ec61d9cd806
Yifan Wang <yif.wang@icloud.com>
2023-04-04 16:20:55 -0700

    chore: catch error within cloudconvert

--- src/utils/cloudconvert.ts
+++ src/utils/cloudconvert.ts
@@ -9,26 +9,31 @@ export const convertFile = async (
   inputFileContent: string,
   outputFileName: string
 ) => {
-  let job = await cloudConvert.jobs.create({
-    tasks: {
-      "import-my-file": {
-        operation: "import/raw",
-        file: inputFileContent,
-        filename: inputFileName,
+  try {
+    let job = await cloudConvert.jobs.create({
+      tasks: {
+        "import-my-file": {
+          operation: "import/raw",
+          file: inputFileContent,
+          filename: inputFileName,
+        },
+        "convert-my-file": {
+          operation: "convert",
+          input: "import-my-file",
+          output_format: "epub",
+          filename: outputFileName,
+        },
+        "export-my-file": {
+          operation: "export/url",
+          input: "convert-my-file",
+        },
       },
-      "convert-my-file": {
-        operation: "convert",
-        input: "import-my-file",
-        output_format: "epub",
-        filename: outputFileName,
-      },
-      "export-my-file": {
-        operation: "export/url",
-        input: "convert-my-file",
-      },
-    },
-  });
-  job = await cloudConvert.jobs.wait(job.id);
-  const file = cloudConvert.jobs.getExportUrls(job)[0];
-  return file;
+    });
+    job = await cloudConvert.jobs.wait(job.id);
+    const file = cloudConvert.jobs.getExportUrls(job)[0];
+    return file;
+  } catch (error) {
+    console.error("Error converting file:", error);
+    return null;
+  }
 };
